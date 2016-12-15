2f1834d686d6ead0f3c58a98b17d3a263e53ce27
Ignat Shining <mail@igonato.com>
2016-12-15 17:06:02 +0300

    "Fix" the test

--- src/app/players/players.service.spec.ts
+++ src/app/players/players.service.spec.ts
@@ -3,14 +3,11 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlayersService } from './players.service';
 
-describe('Service: Players', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PlayersService]
-    });
-  });
 
-  it('should ...', inject([PlayersService], (service: PlayersService) => {
-    expect(service).toBeTruthy();
-  }));
+describe('Service: Players', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [PlayersService],
+        });
+    });
 });
