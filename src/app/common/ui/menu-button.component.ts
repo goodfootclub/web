10a1ea636a9e1b0e4bf9bb1d8fe485b4caf275c3
Ignat Shining <mail@igonato.com>
2016-12-18 15:56:40 +0300

    Fix imports for tests

--- src/app/common/ui/menu-button.component.ts
+++ src/app/common/ui/menu-button.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MenuService } from 'app/common/services/menu.service';
+import { MenuService } from '../services';
 
 
 @Component({
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MenuService } from 'app/common/services';
+import { MenuService } from 'app/common';
 
 
 @Component({
