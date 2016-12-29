f6e2305e48c00fb59371f606f7b8d91f4f8d59cf
Ignat Shining <mail@igonato.com>
2016-12-29 18:51:59 +0300

    Games routes

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleService } from 'app/title.service';
 
 
 @Component({
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-    constructor() { }
+    constructor(public title: TitleService) {
+        title.setTitle('Find a game');
+    }
 
     ngOnInit() {
     }
