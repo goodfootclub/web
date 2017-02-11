f61681f0b021feaa63e7adfae85cd576b42965f5
Ignat Shining <mail@igonato.com>
2017-02-11 23:50:13 +0000

    angular-cli v1.0.0-beta.31

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { GfcPage } from './app.po';
+
+describe('gfc App', () => {
+  let page: GfcPage;
+
+  beforeEach(() => {
+    page = new GfcPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
