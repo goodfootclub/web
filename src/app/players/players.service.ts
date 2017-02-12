eed9e4b5e0e562075eabdc52ee82619a0880841f
bsko <adobeangryslave@gmail.com>
2017-02-12 17:54:10 +0300

    lint warnings fixed

--- src/app/players/players.service.ts
+++ src/app/players/players.service.ts
@@ -23,9 +23,9 @@ export class PlayersService {
 
     all(search?: string, limit?: number, offset?: number): Observable<User[]> {
         const params: URLSearchParams = new URLSearchParams();
-        if(limit) { params.set('limit', limit.toString()); }
-        if(offset) { params.set('offset', offset.toString()); }
-        if(search) { params.set('search', search); }
+        if (limit) { params.set('limit', limit.toString()); }
+        if (offset) { params.set('offset', offset.toString()); }
+        if (search) { params.set('search', search); }
         return this.http.get('/api/users/players/', {search: params})
             .map(res => res.json().results.map(data => new User(data)))
             .catch((err, caught) => {
