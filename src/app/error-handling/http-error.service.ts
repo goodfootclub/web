d127929dbb90250ba1e630ff395898be65876bef
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-03-23 19:45:52 +0300

    error-handling: http error handle with toasty service

--- src/app/error-handling/http-error.service.ts
+++ src/app/error-handling/http-error.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
+import { AppToastyService } from '../common/services/toasty.service';
 
 @Injectable()
 export class HttpErrorHandler {
 
+    constructor(private toastyService: AppToastyService) {}
+
     handleError(error: Response): void {
-        // TODO handle it
+        const status = error.status;
+        const statusText = error.statusText;
+        let message = 'Error on server!';
+        if (error) {
+            const err = error.json();
+            const keys = Object.keys(err);
+            const lines = keys.map((key) => `${key}: ${err[key]}`);
+            message = lines.join('\n');
+        }
+        this.toastyService.error(message);
     }
 
 }
