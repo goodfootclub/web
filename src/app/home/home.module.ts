8c6102f289a354ecab04445deb7ba473e570108b
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-03-29 15:00:37 +0300

    home-view: blocks

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from 'app/common';
 
 import { HomeComponent } from './home.component';
+import { GamesTileComponent } from './games-tile/games-tile.component';
+import { ProfileTileComponent } from './profile-tile/profile-tile.component';
+import { TeamsTileComponent } from './teams-tile/teams-tile.component';
+import { PickupGamesTileComponent } from './pickup-games-tile/pickup-games-tile.component';
+import { PlayersTileComponent } from './players-tile/players-tile.component';
+import { SubsTileComponent } from './subs-tile/subs-tile.component';
+import { ContactusTileComponent } from './contactus-tile/contactus-tile.component';
 
 
 const ROUTES: Routes = [{
@@ -18,6 +25,13 @@ const ROUTES: Routes = [{
     ],
     declarations: [
         HomeComponent,
+        GamesTileComponent,
+        ProfileTileComponent,
+        TeamsTileComponent,
+        PickupGamesTileComponent,
+        PlayersTileComponent,
+        SubsTileComponent,
+        ContactusTileComponent,
     ],
 })
 export class HomeModule { }
