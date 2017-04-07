46cc1f5d7bcbf90f7226978cff7e2bf8232d90b3
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-04-07 15:13:21 +0300

    home-view: rsvp status

--- src/app/home/my-next-game-tile/my-next-game-tile.component.ts
+++ src/app/home/my-next-game-tile/my-next-game-tile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { GameEvent } from '../../types';
+import { GameEvent, RsvpStatus } from '../../types';
 import { ProfileService } from '../../profile/profile.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-my-next-game-tile',
@@ -10,15 +11,27 @@ import { ProfileService } from '../../profile/profile.service';
 export class MyNextGameTileComponent implements OnInit {
 
     nextGame: GameEvent;
+    rsvpStatus: RsvpStatus;
+
+    rsvpMessages = {
+        2: 'In',
+        1: 'Maybe',
+        0: 'Out',
+    };
 
     constructor(
         private profileService: ProfileService,
+        private authService: AuthService,
     ) { }
 
     ngOnInit() {
         this.profileService.getCurrentUserGames(1).subscribe(data => {
             if (data && data instanceof Array && data.length > 0) {
                 this.nextGame = data[0];
+                const user = this.nextGame.playersById[
+                    this.authService.profile.currentUser.id
+                ];
+                this.rsvpStatus = user.rsvp;
             }
         });
     }
