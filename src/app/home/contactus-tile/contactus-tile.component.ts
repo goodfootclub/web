f723f24f256a95ba3edbc30bd68497060aa59ab2
Ignat Shining <mail@igonato.com>
2017-04-17 19:08:14 +0100

    Contact Us sends to Facebook

--- src/app/home/contactus-tile/contactus-tile.component.ts
+++ src/app/home/contactus-tile/contactus-tile.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
     selector: 'app-contactus-tile',
     templateUrl: './contactus-tile.component.html',
     styleUrls: [
-      './contactus-tile.component.styl',
-    ]
+        './contactus-tile.component.styl',
+    ],
 })
 export class ContactusTileComponent implements OnInit {
 
