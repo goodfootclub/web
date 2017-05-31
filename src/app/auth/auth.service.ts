900cab3a68e80ded08fc3086d15de693d507a38e
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-05-31 10:46:40 +0300

    isAuthenticated check w/o request to BE

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,18 +6,20 @@ import {
     Router,
     RouterStateSnapshot,
 } from '@angular/router';
+import { ProfileService } from '../profile';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 
-import { User } from '../types';
-import { ProfileService } from '../profile';
 
 
 @Injectable()
 export class AuthService implements CanActivate, CanActivateChild {
+
+    private readonly csrfCookie = 'csrftoken';
 
     activationsChecks = 0;
     nextUrl: string;  // store the URL so we can redirect after logging in
@@ -42,17 +44,11 @@ export class AuthService implements CanActivate, CanActivateChild {
      * @return {boolean | Observable<boolean>}
      */
     isAuthenticated(redirectPath = 'signup'): boolean | Observable<boolean> {
-
-        // Helper function checks if user is null and may redirect to
-        // the app's login page
-        let checkAndRedirect = user => {
-            if (user == null && redirectPath != null) {
-                this.router.navigate([redirectPath]);
-            }
-            return user != null;
-        };
-
-        let user = this.profile.getCurrentUser();
-        return (user as Observable<User>).map(checkAndRedirect);
+        const csrf = Cookie.get(this.csrfCookie);
+        const isAuthenticated = !!csrf;
+        if (!isAuthenticated) {
+            this.router.navigate([redirectPath]);
+        }
+        return isAuthenticated;
     }
 }
