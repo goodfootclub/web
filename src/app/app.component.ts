3cdeb39aa379160c7ded6ac5c7746bade21647ad
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-06-29 21:05:35 +0300

    fixed window size when starting from non home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TitleService } from './title.service';
 import { AnalyticsService } from './common';
 import { StatusService } from './common/services/status.service';
 import { HistoryService } from './common/services/history.service';
+import { AuthService } from './auth/auth.service';
 import { WindowRefService } from './common/services/window.service';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.styl'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     menu: any;
 
     isDevelopment = !!environment.development;
@@ -20,9 +22,20 @@ export class AppComponent {
         public title: TitleService,
         public status: StatusService,
         public windowRef: WindowRefService,
+        private authService: AuthService,
         private analytics: AnalyticsService,
         private historyService: HistoryService,
     ) {
         title.setTitle('Your Games');
+    }
+
+    ngOnInit(): void {
+        const authenticated = this.authService.isAuthenticated();
+        if (authenticated instanceof Observable) {
+            (authenticated as Observable<boolean>)
+                .subscribe(result => this.windowRef.setFullScreen(!result));
+        } else {
+            this.windowRef.setFullScreen(!(authenticated as boolean));
+        }
     }
 }
