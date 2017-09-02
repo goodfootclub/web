ded93facd4cef6fb1a60051524747a30fffe3c00
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-09-02 10:11:21 +0300

    registration tests fixed

--- src/app/auth/logout/logout.component.spec.ts
+++ src/app/auth/logout/logout.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
+import { WindowRefService } from '../../common/services/window.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LogoutComponent', () => {
-  let component: LogoutComponent;
-  let fixture: ComponentFixture<LogoutComponent>;
+    let component: LogoutComponent;
+    let fixture: ComponentFixture<LogoutComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LogoutComponent ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LogoutComponent],
+            imports: [
+                RouterTestingModule,
+            ],
+            providers: [
+                { provide: WindowRefService, useClass: WindowRefServiceStub },
+            ],
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LogoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LogoutComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+class WindowRefServiceStub {
+    setFullScreen(flag) {}
+}
