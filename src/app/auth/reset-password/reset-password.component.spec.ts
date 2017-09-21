7dbd3e324b17d998f12378bbaef33e76ae24eab9
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-09-21 11:29:03 +0300

    tests fixed

--- src/app/auth/reset-password/reset-password.component.spec.ts
+++ src/app/auth/reset-password/reset-password.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ResetPasswordComponent } from './reset-password.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileService } from '../../profile/profile.service';
 
 describe('ResetPasswordComponent', () => {
-  let component: ResetPasswordComponent;
-  let fixture: ComponentFixture<ResetPasswordComponent>;
+    let component: ResetPasswordComponent;
+    let fixture: ComponentFixture<ResetPasswordComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ResetPasswordComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ResetPasswordComponent ],
+            imports: [
+                ReactiveFormsModule,
+                RouterTestingModule,
+            ],
+            schemas: [ NO_ERRORS_SCHEMA ],
+            providers: [
+                { provide: ProfileService, useClass: ProfileServiceStub },
+            ],
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ResetPasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ResetPasswordComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+class ProfileServiceStub {}
