2fe8ea74aca7561e9f428343ab80445b04b1ea14
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-10-08 16:16:35 +0300

    basic tests fixed

--- src/app/games/my-games/my-games.component.spec.ts
+++ src/app/games/my-games/my-games.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyGamesComponent } from './my-games.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { GamesService } from '../games.service';
+import { TitleService } from '../../core/services/title.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('MyGamesComponent', () => {
-  // let component: MyGamesComponent;
-  // let fixture: ComponentFixture<MyGamesComponent>;
-  //
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ MyGamesComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(MyGamesComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-  //
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+    let component: MyGamesComponent;
+    let fixture: ComponentFixture<MyGamesComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ MyGamesComponent ],
+            schemas: [ NO_ERRORS_SCHEMA ],
+            providers: [
+                { provide: GamesService, useClass: GameServiceStub },
+                { provide: TitleService, useClass: TitleServiceStub },
+            ],
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MyGamesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+class GameServiceStub {
+    getCurrentUserGames = () => Observable.of({ results: [] });
+}
+
+class TitleServiceStub {
+    setTitle = () => {}
+}
--- src/app/players/players.component.spec.ts
+++ src/app/players/players.component.spec.ts
@@ -1,8 +1,52 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { PlayersComponent } from './players.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { PlayersService } from './players.service';
+import { GamesService } from '../games/games.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TeamsService } from '../teams/teams.service';
+import { TitleService } from '../core/services/title.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('Component: Players', () => {
+    let component: PlayersComponent;
+    let fixture: ComponentFixture<PlayersComponent>;
 
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ PlayersComponent ],
+            schemas: [ NO_ERRORS_SCHEMA ],
+            imports: [ ReactiveFormsModule, RouterTestingModule ],
+            providers: [
+                { provide: PlayersService, useClass: PlayersServiceStub },
+                { provide: GamesService, useClass: GamesServiceStub },
+                { provide: TeamsService, useClass: TeamsServiceStub },
+                { provide: TitleService, useClass: TitleServiceStub },
+            ],
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PlayersComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+class PlayersServiceStub {
+    all = () => Observable.of([]);
+}
+
+class GamesServiceStub {}
+
+class TeamsServiceStub {}
+
+class TitleServiceStub {
+    setTitle = () => {}
+}
