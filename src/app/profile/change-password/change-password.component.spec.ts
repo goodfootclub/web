24fba00e9737bc3c31a2cbc2c6a00e5c9b9f699a
atemnov <Alexey.Temnov@bsc-ideas.com>
2017-10-17 08:14:34 +0300

    change password view: tests fixed

--- src/app/profile/change-password/change-password.component.spec.ts
+++ src/app/profile/change-password/change-password.component.spec.ts
@@ -1,25 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangePasswordComponent } from './change-password.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TitleService } from '../../core/services/title.service';
+import { HistoryService } from '../../core/services/history.service';
 
 describe('ChangePasswordComponent', () => {
-  let component: ChangePasswordComponent;
-  let fixture: ComponentFixture<ChangePasswordComponent>;
+    let component: ChangePasswordComponent;
+    let fixture: ComponentFixture<ChangePasswordComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ChangePasswordComponent ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ChangePasswordComponent],
+            imports: [ReactiveFormsModule],
+            providers: [
+                { provide: TitleService, useClass: TitleServiceStub },
+                { provide: HistoryService, useClass: HistoryServiceStub },
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChangePasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ChangePasswordComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+class TitleServiceStub {
+    setTitle = () => {};
+}
+
+class HistoryServiceStub {}
